8607c732884bf4b370ba3803008e6da00eadf2ec
David <david.p646@gmail.com>
2025-12-01 14:33:56 -0800

    day1p2

--- day01.py
+++ day01.py
@@ -22,7 +22,7 @@ for i in data:
         pos += i[1]
     else:
         pos -= i[1]
-    pos = pos%100
+    pos = pos % 100
     if pos == 0:
         password += 1
 
@@ -31,7 +31,30 @@ print(password)
 # ----------------------------------------------------------------------------------------------------------------------
 # Part 2
 
+pos = 50
+password = 0
+for i in data:
+    oldpos = pos
+    if i[0] == 'R':
+        pos += i[1]
+    elif i[0] == 'L':
+        pos -= i[1]
+    #print(f'pos: {pos}, oldpos: {oldpos}')
+
+    # Add for crossing 0
+    if pos < 0 < oldpos:
+        password += 1
+
+    # Add rotations > 100
+    if (abs(pos) // 100) > 0:
+        password += abs(pos) // 100
+
+    # Add pointing at 0
+    if pos == 0:
+        password += 1
+
+    pos = pos % 100
+
 
 print("Part two: ")
-print()
-
+print(password)
